d46e26141cc3f7868176b6ca49662dd556b3fad4
Shift <shift@laravelshift.com>
2023-03-28 00:38:09 +0000

    Use ES modules

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
-window._ = require('lodash');
+import _ from 'lodash';
+window._ = _;
 
 try {
     window.Popper = require('popper.js').default;
@@ -13,7 +14,8 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+import axios from 'axios';
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
